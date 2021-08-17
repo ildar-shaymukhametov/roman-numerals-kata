16d7f0b98e82c9819bf036e5a075dc1936bf77ce
Шаймухаметов Ильдар <ildar.shaymukhametov@gmail.com>
2021-08-17 14:08:14 +0500

    replace method with dictionary

--- src/Converter.cs
+++ src/Converter.cs
@@ -1,5 +1,40 @@
 public static class Converter
 {
+    private static readonly Dictionary<int, string> _data = new Dictionary<int, string>
+    {
+        [0] = "",
+        [1] = "I",
+        [2] = "II",
+        [3] = "III",
+        [4] = "IV",
+        [5] = "V",
+        [6] = "VI",
+        [7] = "VII",
+        [8] = "VIII",
+        [9] = "IX",
+        [10] = "X",
+        [20] = "XX",
+        [30] = "XXX",
+        [40] = "XL",
+        [50] = "L",
+        [60] = "LX",
+        [70] = "LXX",
+        [80] = "LXXX",
+        [90] = "XC",
+        [100] = "C",
+        [200] = "CC",
+        [300] = "CCC",
+        [400] = "CD",
+        [500] = "D",
+        [600] = "DC",
+        [700] = "DCC",
+        [800] = "DCCC",
+        [900] = "CM",
+        [1000] = "M",
+        [2000] = "MM",
+        [3000] = "MMM",
+    };
+
     public static string ToRoman(int n)
     {
         var result = "";
@@ -10,48 +45,10 @@ public static class Converter
             .ForEach(numOfDigits =>
             {
                 var number = remainder % (int)Math.Pow(10, numOfDigits);
-                result = result.Insert(0, ToRomanNumeral(number));
+                result = result.Insert(0, _data[number]);
                 remainder -= number;
             });
 
         return result;
-    }
-
-    private static string ToRomanNumeral(int n)
-    {
-        return n switch
-        {
-            1 => "I",
-            2 => "II",
-            3 => "III",
-            4 => "IV",
-            5 => "V",
-            6 => "VI",
-            7 => "VII",
-            8 => "VIII",
-            9 => "IX",
-            10 => "X",
-            20 => "XX",
-            30 => "XXX",
-            40 => "XL",
-            50 => "L",
-            60 => "LX",
-            70 => "LXX",
-            80 => "LXXX",
-            90 => "XC",
-            100 => "C",
-            200 => "CC",
-            300 => "CCC",
-            400 => "CD",
-            500 => "D",
-            600 => "DC",
-            700 => "DCC",
-            800 => "DCCC",
-            900 => "CM",
-            1000 => "M",
-            2000 => "MM",
-            3000 => "MMM",
-            _ => ""
-        };
     }
 }
